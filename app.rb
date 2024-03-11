0c49952d57e79a00f7d60cdeed8f594756b4840f
Jimmy Poulsen <jimmy@poulsen.dk>
2024-03-11 19:42:03 +0100

    Submit access token and list repositories

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require "sinatra"
+require "octokit"
 
 get "/" do
-  "Hello, World!"
+  erb :index
+end
+
+get "/repositories" do
+  access_token = params[:fine_grained_access_token]
+  @client = Octokit::Client.new(access_token: access_token)
+  @repositories = @client.repositories
+
+  erb :repositories
 end
